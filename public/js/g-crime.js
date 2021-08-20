2f977aa2508f690862ee0434e0dca686cb7d6f3a
helamandc <manlehcastillo@gmail.com>
2021-08-21 05:38:57 +1000

    Updated API key with genre.

--- public/js/g-crime.js
+++ public/js/g-crime.js
@@ -1,26 +1,23 @@
 const base_URL = "https://api.themoviedb.org/3"
-const api_key = "?api_key=1cb0f23eb9774f53a3ad114e334ff695"
+const api_key = "?api_key=1cb0f23eb9774f53a3ad114e334ff695&with_genres="
 const image_URL = "https://image.tmdb.org/t/p/w185"
 
-$.getJSON(base_URL + "/discover/movie" + api_key)
+$.getJSON(base_URL + "/discover/movie" + api_key + 80)
   .then(data => {
     console.log(data)
 
     const movies = data.results
     for (let i = 0; i < movies.length; i++) {
       const movie = movies[i]
-      if (movie.genre_ids.includes(80)) {
         const movieHTML = $("<div>", { class: "movie-tile" })
           .append(`<h5 class="movie-title">${movie.title}`)
-          .append(`<p class="ave-rating">Overall rating (# votes):</p>`)
+          .append(`<p class="ave-rating"><span class="fa fa-star checked"></span>&nbsp( ) from # votes</p>`)
           .append(
             `<img src="${image_URL + movie.poster_path}" alt="${movie.title
             } poster">`
           )
 
         $("#insert-here").append(movieHTML)
-      }
-
     }
   })
   .catch(err => {
